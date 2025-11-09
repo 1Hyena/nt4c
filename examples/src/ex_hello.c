93f30f70addca415dec6d5af64613197c9e68ccf
1Hyena <hyena@hyena.net.ee>
2025-11-09 21:20:55 +0200

    initial placeholder code

---
+++ examples/src/ex_hello.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+#include "../../nt4c.h"
+#include <stdlib.h>
+
+
+int main(int, char **) {
+    return EXIT_SUCCESS;
+}
---
+++ nt4c.h
@@ -0,0 +1,5 @@
+#ifndef NT4C_H_09_11_2025
+#define NT4C_H_09_11_2025
+
+
+#endif
