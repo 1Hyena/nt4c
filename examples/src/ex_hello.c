2c883e31b79b066b97e61af2a43d6bde42d2557e
1Hyena <hyena@hyena.net.ee>
2025-11-10 00:47:48 +0200

    simplified the hello world example code

--- examples/src/ex_hello.c
+++ examples/src/ex_hello.c
@@ -18,10 +18,11 @@ static void dump_node(NT_NODE *node, size_t depth) {
 }
 
 int main(int, char **) {
-    NT_NODE nodes[32];
+    constexpr size_t node_count = 8;
+    NT_NODE nodes[node_count];
     NT_PARSER parser = {};
 
-    nt_parser_set_memory(&parser, nodes, (sizeof(nodes) / sizeof((nodes)[0])));
+    nt_parser_set_memory(&parser, nodes, node_count);
     nt_parse("hello: world", 0, &parser);
     dump_node(parser.node.root, 0);
 
