3f37a98a65415d91b73453160f30d28ebbc91a55
Erich Erstu <mail@ErichErstu.net>
2026-05-01 17:52:20 +0300

    no longer relying on global variable

--- examples/src/ex_query.c
+++ examples/src/ex_query.c
@@ -5,16 +5,14 @@
 
 static int callback(NT_TYPE, const char *, size_t, void *, size_t);
 
-static struct {
-    struct {
-        const char *key;
+typedef struct QUERY {
+    const char *key;
 
-        struct {
-            size_t depth;
-            NT_TYPE type;
-        } result;
-    } query;
-} global;
+    struct {
+        size_t depth;
+        NT_TYPE type;
+    } result;
+} QUERY;
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -22,7 +20,9 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    global.query.key = argv[1];
+    QUERY query = {};
+
+    query.key = argv[1];
 
     size_t input_size;
     char *input_data = read_file_to_memory(argv[2], &input_size);
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]) {
     NT_PARSER parser = nt_make_parser();
 
     nt_parser_set_callback(&parser, callback);
+    nt_parser_set_userdata(&parser, &query);
     nt_parser_set_blacklist(&parser, NT_INTERSPACE);
     nt_parser_parse(&parser, input_data, input_size);
 
@@ -42,31 +43,34 @@ int main(int argc, char *argv[]) {
 }
 
 static int callback(
-    NT_TYPE node_type, const char *data, size_t data_size, void *, size_t depth
+    NT_TYPE node_type, const char *data, size_t data_size,
+    void *userdata, size_t depth
 ) {
+    QUERY *query = (QUERY *) userdata;
+
     NT_TYPE group_type = nt_type_type(node_type); // Let's generalize the type.
 
     if (group_type == NT_KEY
-    && !global.query.result.depth
-    && !strncmp(data, global.query.key, data_size)
-    && strlen(global.query.key) == data_size) {
-        global.query.result.depth = depth;
+    && !query->result.depth
+    && !strncmp(data, query->key, data_size)
+    && strlen(query->key) == data_size) {
+        query->result.depth = depth;
 
         return 0;
     }
 
-    if (depth <= global.query.result.depth) {
+    if (depth <= query->result.depth) {
         return -1;
     }
 
-    if (!global.query.result.depth) {
+    if (!query->result.depth) {
         return 0;
     }
 
-    if (global.query.result.type == NT_NONE) {
+    if (query->result.type == NT_NONE) {
         if ((group_type & (NT_STR|NT_KEY|NT_TAG_LST)) == false) {
             if (group_type == NT_STR_COM) {
-                for (size_t i = global.query.result.depth + 1; i < depth; ++i) {
+                for (size_t i = query->result.depth + 1; i < depth; ++i) {
                     printf("%s", "    ");
                 }
 
@@ -76,11 +80,11 @@ static int callback(
             return 0;
         }
 
-        global.query.result.type = group_type;
+        query->result.type = group_type;
     }
 
-    if (global.query.result.type == NT_STR) {
-        if (group_type == NT_STR && depth == global.query.result.depth + 1) {
+    if (query->result.type == NT_STR) {
+        if (group_type == NT_STR && depth == query->result.depth + 1) {
             printf("%.*s\n", (int) data_size, data);
         }
 
@@ -88,7 +92,7 @@ static int callback(
     }
 
     if (node_type == NT_INDENT) {
-        for (size_t i = global.query.result.depth + 1; i < depth; ++i) {
+        for (size_t i = query->result.depth + 1; i < depth; ++i) {
             printf("%s", "    ");
         }
     }
